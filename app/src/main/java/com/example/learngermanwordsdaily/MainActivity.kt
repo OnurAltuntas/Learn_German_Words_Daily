4110bc10887cf8e7c227718b294d9412a4ce5386
OnurAltuntas <onuraltuntas50@gmail.com>
2019-09-03 22:46:54 +0300

    Daily limit is done.(working properly on api 21+)

--- app/src/main/java/com/example/learngermanwordsdaily/MainActivity.kt
+++ app/src/main/java/com/example/learngermanwordsdaily/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.learngermanwordsdaily
 import GifDrawableImageViewTarget
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.random.Random
 
 
@@ -18,14 +26,38 @@ class MainActivity : AppCompatActivity() {
     var ran=0
     var loop=10
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.activity_main)
+
         var veri=Veri()
 
+        var sharedPreferences = this.getSharedPreferences(packageName, android.content.Context.MODE_PRIVATE)
 
+        var formatter = SimpleDateFormat("dd/MM/yyyy")
+
+
+        var installday =formatter.parse(formatter.format( Date()))
+
+        var showDate = sharedPreferences.getString("DATE","none")
+
+
+        var str1=installday.toString()
+        var str2=showDate.toString()
+
+        if(str1.equals(str2)){
+            loop=0
+            txtBound.text="0"
+
+        }
+        else{
+            loop=10
+            txtBound.text="10"
+        }
         btnGo.setOnClickListener {
+
             if(loop>0){
                 btnGo.isClickable=false
                 Glide.with(this).load(R.drawable.giphy).into(GifDrawableImageViewTarget(imgWheel, 1))
@@ -44,13 +76,10 @@ class MainActivity : AppCompatActivity() {
 
                 if(loop==0){
                     Toast.makeText(applicationContext,"You have reached your daily limit!",Toast.LENGTH_LONG).show()
+                    sharedPreferences.edit().putString("DATE",installday.toString()).apply()
                 }
 
-
-                // btnGo.isClickable=true
             }
-
-
 
         }
 
@@ -60,8 +89,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
     }
 
-
 }
+
+
+
